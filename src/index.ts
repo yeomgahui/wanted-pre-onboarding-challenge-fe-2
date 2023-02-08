ddf4a8d9053dc79ac2af38db50c474821b8acb1c
gahui yeom <ykh4933@naver.com>
2023-02-08 23:26:47 +0900

    seconde requirement complate

--- src/index.ts
+++ src/index.ts
@@ -1,132 +1,39 @@
 interface Todo {
 	readonly id: string;
-	contents: string;
-	category: string;
-	isDone: boolean;
-	tags: string[];
+	contents :string;
+	isCompleted: boolean;
+	category ?: string;
+	tags ?: string[];
 }
+
 
 interface CreateTodo extends Todo {
-	isDone: false;
+	isCompleted : false;
 }
 
-class Todos {
-	todoList: Todo[];
-	constructor() {
-		this.todoList = [];
-	}
 
-	addTodo(todo:Omit<CreateTodo, 'id' | 'isDone'>){
-		const todoItem: CreateTodo =  {
-			id: String(Date.now()),
-			contents: todo.contents,
-			isDone: false,
-			category: todo.category,
-			tags: todo.tags
-		}
-		this.todoList.push(todoItem);
-		return todoItem;
-	}
+// 할 일을 추가할 수 있다.
+// 내용없이 추가할 수 없다.
+const addTodo = (todo: Omit<CreateTodo, 'id' | 'isCompleted'>) => {}
 
-	readTodoById(selectedId: Todo['id']){
-		return this.todoList.filter( todo =>  todo.id === selectedId);
-	}
-	readAllTodo(){
-		return this.todoList;
-	}
+// ID를 제외한 모든 속성을 수정할 수 있다.
+// 특정 할 일의 특정 태그를 수정할 수 있다.
+const updateTodoById = (selectedId: Todo['id'], modifiedTodo : Partial<Omit<Todo, 'id'>>) => {}
+const updateTodoTagById = (selectedId: Todo['id'], oldTag:string, newTag:string) => {}
 
-	updateTodoById(selectedId: Todo['id'], fieldsUpdate: Partial<Pick<Todo, 'contents' | 'category'>>){
-		const targetIndex = this.todoList.findIndex(todo=> todo.id === selectedId);
-		if(targetIndex < 0){
-			throw new Error('Cannot find Todo Item');
-		}
-		return this.todoList[targetIndex] = {...this.todoList[targetIndex], ...fieldsUpdate};
-	}
+// ID를 기반으로 특정 할 일을 삭제할 수 있다.
+// 모든 할 일을 제거할 수 있다.
+// 특정 할 일의 특정 태그를 삭제할 수 있다.
+// 특정 할 일의 모든 태그를 제거할 수 있다.
+const deleteTodoById = (selectedId: Todo['id']) => {}
+const deleteTodoAll = () => {}
 
-	toggleIsDone(selectedId: Todo['id']){
-		const targetItem = this.todoList.find(todo => todo.id === selectedId);
-		if(!targetItem){
-			throw Error('Cannot find Todo Item');
-		}
-		targetItem.isDone = !targetItem.isDone;
-		return targetItem;
-	}
+const deleteTagAll = () => {}
+const deleteTagByTagName = (selectedId: Todo['id'], tagName: string) => {}
 
-	updateTodoTagById(selectedId: Todo['id'], oldTag: string, newTag:string){
-		const targetItem = this.todoList.find(todo => todo.id === selectedId);
-		if(!targetItem){
-			throw Error('Cannot find Todo Item');
-		}
-		const targetTagIndex = targetItem.tags.findIndex(tag => tag === oldTag);
-		if(targetTagIndex < 0){
-			throw Error('Cannot find tag');
-		}
-		targetItem.tags[targetTagIndex] = newTag;
-		return targetItem;
-	}
-	deleteTodoById(selectedId: Todo['id']){
-		const targetIndex = this.todoList.findIndex(todo=> todo.id === selectedId);
-		if(targetIndex < 0){
-			throw new Error('Cannot find Todo Item');
-		}
-		this.todoList.splice(targetIndex,1);
-		return this.todoList;
-	}
 
-	deleteAllTodo(){
-		this.todoList = [];
-		return this.todoList;
-	}
-
-	deleteTodoTag(selectedId: Todo['id'], tagName: string){
-		const targetItem = this.todoList.find(todo=> todo.id === selectedId);
-		if(!targetItem){
-			throw new Error('Cannot find Todo Item');
-		}
-		const targetTagIndex = targetItem.tags.indexOf(tagName);
-		if(targetTagIndex < 0){
-			throw new Error('Cannot find Todo Tag');
-		}
-		targetItem.tags.splice(targetTagIndex, 1);
-		return targetItem;
-	}
-
-	deleteAllTodoTag(selectedId: Todo['id']){
-		const targetItem = this.todoList.find(todo=> todo.id === selectedId);
-		if(!targetItem){
-			throw new Error('Cannot find Todo Item');
-		}
-		targetItem.tags = [];
-		return targetItem;
-	}
-
-}
-
-const todoInstance = new Todos();
-
-const newTodo = {
-	contents: '타입스크립트',
-	category: 'work',
-	tags: ['monday', 'tuesday']
-}
-const newTodo2 = {
-	contents: '자바',
-	category: 'work',
-	tags: ['monday', 'wendesday']
-}
-const todoItem = todoInstance.addTodo(newTodo);
-const todoItem2 = todoInstance.addTodo(newTodo2);
-console.log(todoInstance.readAllTodo());
-
-//-------------------------------------------------------
-
-// todoInstance.updateTodoById(todoItem.id, { contents: 'coding'});
-// todoInstance.toggleIsDone('todoItem.id');
-todoInstance.updateTodoTagById(todoItem.id,'monday', 'sunday')
-console.log(todoInstance.readAllTodo());
-
-//----------------------------------------------------
-// todoInstance.deleteTodoById(todoItem.id);
-todoInstance.deleteTodoTag(todoItem.id, "sunday");
-console.log(todoInstance.readAllTodo());
+// 모든 할 일을 조회할 수 있다.
+// ID를 기반으로 특정 할 일을 조회할 수 있다.
+const readTodoById = (selectedId: Todo['id']) => {}
+const readTodoAll = () => {}
 
